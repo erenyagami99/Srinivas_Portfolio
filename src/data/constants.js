efd769cc8f686828eb8cecdfd737d01e83e8b4a7
srinivas7661 <srinivas@leewayhertz.com>
2023-09-16 20:59:52 +0530

    new project

--- src/data/constants.js
+++ src/data/constants.js
@@ -224,33 +224,32 @@ export const projects = [
     // github: "https://github.com/rishavchanda/Trackify",
     webapp: "https://srinivas-website.netlify.app/",
   },
-  // {
-  //   id: 0,
-  //   title: "Podstream",
-  //   date: "Apr 2023 - May 2023",
-  //   description:
-  //     "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
-  //   image:
-  //     "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
-  //   tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
-  //   category: "web app",
-  //   github: "https://github.com/rishavchanda/Podstream",
-  //   webapp: "https://podstream.netlify.app/",
-  //   member: [
-  //     {
-  //       name: "Rishav Chanda",
-  //       img: "https://avatars.githubusercontent.com/u/64485885?v=4",
-  //       linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-  //       github: "https://github.com/rishavchanda/",
-  //     },
-  //     {
-  //       name: "Upasana Chaudhuri",
-  //       img: "https://avatars.githubusercontent.com/u/100614635?v=4",
-  //       linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
-  //       github: "https://github.com/upasana0710",
-  //     },
-  //   ],
-  // },
+  {
+    id: 0,
+    title: "Task Board",
+    date: "Sep 2023 - Sep 2023",
+    description:
+      "Developed a MEAN Stack web application that allows admins to create stages which includes task and assign them to the employees. The stages are dynamic admin can create a new stage update and delete it, same goest with the tasks. Drag and drop is implemented in this project so that tasks can be moved from one stage to other dynamically. NgRx store is implemented to manage user data.",
+    image: "https://www.linkpicture.com/q/task-board.png",
+    tags: ["Angular", "MongoDb", "Node Js", "Express Js", "NgRx", "Postman"],
+    category: "web app",
+    // github: "https://github.com/rishavchanda/Podstream",
+    webapp: "https://task-board-srinivas.netlify.app/",
+    // member: [
+    //   {
+    //     name: "Rishav Chanda",
+    //     img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+    //     linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+    //     github: "https://github.com/rishavchanda/",
+    //   },
+    //   {
+    //     name: "Upasana Chaudhuri",
+    //     img: "https://avatars.githubusercontent.com/u/100614635?v=4",
+    //     linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+    //     github: "https://github.com/upasana0710",
+    //   },
+    // ],
+  },
   // {
   //   id: 1,
   //   title: "Vexa",
